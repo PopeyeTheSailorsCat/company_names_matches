e0ab1e2bafcf3f7f9aca6ee620e735d2466bfb92
Dmitry Vedenichev <dmitroved@gmail.com>
2022-10-25 01:09:17 +0300

    update db initialisation

--- db/data_base.py
+++ db/data_base.py
@@ -4,19 +4,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from qdrant_client import QdrantClient
 from qdrant_client.http import models as rest
-from qdrant_client.http.models import models
+from qdrant_client.http.models import models as qdrant_models
 from db.client_class import DataBaseClient
 
 LOCAL_HOST = "127.0.0.1"
 PORT = 6333
-COL_NAME = 'companies'
+COL_NAME = 'companies-ft'
 
 
 # LOCAL_HOST = "0.0.0.0"
 def create_collection_and_upload(vectors, payload, ids, col_name, vec_shape, bs):
     qdrant_client.recreate_collection(collection_name=col_name,
-                                      vectors_config=models.VectorParams(size=vec_shape,
-                                                                         distance=models.Distance.COSINE),
+                                      vectors_config=qdrant_models.VectorParams(size=vec_shape,
+                                                                         distance=qdrant_models.Distance.COSINE),
                                       on_disk_payload=True)
     qdrant_client.upload_collection(
         collection_name=col_name,
@@ -35,6 +35,8 @@ def get_qdrant_client_():
 
 
 if __name__ == "__main__":
+    embeddings = np.load('data/tuned/tuned_2.npy')
+
     df_emb = pd.read_parquet('data/df_embs_preproc.parquet')  # TODO to new version
 
     df_names = pd.read_parquet('data/df_names_preproc.parquet')
@@ -44,10 +46,10 @@ if __name__ == "__main__":
 
     qdrant_client = QdrantClient(host=LOCAL_HOST, port=PORT)
     #
-    vectors = np.stack(df_names.emb)
+    vectors = embeddings
     payload = df_names[['original_name', 'preprocessed_name']].to_dict(orient='records')
     ids = df_names.index.values.tolist()
-    col_name = 'companies'
+    col_name = COL_NAME
     vec_shape = vectors.shape[1]
     bs = 1024
     col = create_collection_and_upload(vectors, payload, ids, col_name, vec_shape, bs)
@@ -56,3 +58,10 @@ if __name__ == "__main__":
 
     # col = qdrant_client.get_collection(col_name)
     # print(col.dict())
+
+# payload = df_names[['original_name', 'preprocessed_name']].to_dict(orient='records')
+# # ids = df_emb.index.values.tolist()
+# col_name = 'companies-ft'
+# vec_shape = vectors.shape[1]
+# bs = 1024
+# col = create_collection_and_upload(vectors, payload, ids, col_name, vec_shape, bs)
